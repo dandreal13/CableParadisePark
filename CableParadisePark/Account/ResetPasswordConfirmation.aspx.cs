e8ba86afd71f93392f5e73679f07116b8592842a
D'Andrea <dandreal13@students.ecu.edu>
2018-03-19 16:59:36 +0000

    Add project files.

---
+++ CableParadisePark/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CableParadisePark.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CableParadisePark/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CableParadisePark.Startup))]
+namespace CableParadisePark
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
